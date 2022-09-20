03a0c517b19685aa305d808ee4de3927ff66245a
Nelfyk <ruslan-burduzhan@mail.ru>
2022-09-20 13:44:33 +0500

    add direction String and small collision physics

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,21 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel{
-    Timer timer;
-    int x = 0;
+    private final int WIDTH = 600;
+    private final int HEIGHT = 600;
+    private final Timer timer;
+    private int x = 0;
+    private int y = 0;
+    private final int step = 1;
+    String direction = "right";
 
     GamePanel() {
         timer = new Timer(1, e->{
+            update();
             repaint();
-            System.out.println(x);
-            if(x<600-30)
-                x++;
-            else {
-                timer.stop();
-                System.exit(0);
-            }
         });
-        this.setSize(600,600);
+
+        this.addKeyListener(new KeyHandler(this));
+        this.setFocusable(true);
+        this.setSize(WIDTH,HEIGHT);
         this.setBackground(Color.black);
         timer.start();
 
@@ -25,7 +27,28 @@ public class GamePanel extends JPanel{
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        g.drawRect(x, 20, 30, 30);
+        g.drawRect(x, y, 30, 30);
         g.dispose();
     }
+
+    public void update(){
+        switch (direction){
+            case "left" -> x-=step;
+            case "up" -> y-=step;
+            case "right" -> x+=step;
+            case "down" -> y+=step;
+        }
+        // COLLISION
+        if(x+50>WIDTH || x<0 || y+50>HEIGHT || y<0){
+            endGame();
+        }
+    }
+    public void endGame(){
+        System.exit(1);
+    }
 }
+
+/*
+apple
+tail
+*/
--- src/KeyHandler.java
+++ src/KeyHandler.java
@@ -1,3 +1,45 @@
-public class KeyHandler {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+public class KeyHandler implements KeyListener {
+    GamePanel gp;
+    KeyHandler(GamePanel gp){
+        this.gp=gp;
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println(e.getKeyCode());
+        switch(e.getKeyCode()){
+            case 37 -> {
+                if(!gp.direction.equals("right")) {
+                    gp.direction = "left";
+                }
+            }
+            case 38 -> {
+                if(!gp.direction.equals("down")) {
+                    gp.direction = "up";
+                }
+            }
+            case 39 -> {
+                if(!gp.direction.equals("left")) {
+                    gp.direction = "right";
+                }
+            }
+            case 40 -> {
+                if(!gp.direction.equals("up")) {
+                    gp.direction = "down";
+                }
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
